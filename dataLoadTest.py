73b0f75ebbcf6ae69b8940835ee384d7bcd18417
KingCol13 <kingsleydgs@gmail.com>
2020-12-05 15:30:27 +0000

    Started data loading code.

--- dataLoadTest.py
+++ dataLoadTest.py
@@ -6,3 +6,47 @@ Created on Fri Dec  4 14:21:07 2020
 @author: kingsley
 """
 
+#%% imports
+
+import uproot 
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from pylorentz import Momentum4
+from pylorentz import Position4
+from lbn import LBN, LBNLayer
+
+# stop tensorflow trying to overfill GPU memory
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+  except RuntimeError as e:
+    print(e)
+    
+#%% Data loading
+
+tree = uproot.open("MVAFILE_AllHiggs_tt.root")["ntuple"]
+
+momenta_features = [ "pi_E_1", "pi_px_1", "pi_py_1", "pi_pz_1", #leading charged pi 4-momentum
+              "pi_E_2", "pi_px_2", "pi_py_2", "pi_pz_2", #subleading charged pi 4-momentum
+              "pi0_E_1","pi0_px_1","pi0_py_1","pi0_pz_1", #leading neutral pi 4-momentum
+              "pi0_E_2","pi0_px_2","pi0_py_2","pi0_pz_2"] #subleading neutral pi 4-momentum
+
+selectors = [ "tau_decay_mode_1","tau_decay_mode_2",
+             "mva_dm_1","mva_dm_2"
+             ]
+
+df = tree.pandas.df(momenta_features+target+selectors
+                    +neutrino_features+met_features+sv_features+sv_features+ip_features
+                    +phi_cp_feature)
+
+for df in uproot3.pandas.iterate("MVAFILE_AllHiggs_tt.root", entrysteps=500):
+    df = df[
+      (df["tau_decay_mode_1"] == 1) 
+    & (df["tau_decay_mode_2"] == 1) 
+    & (df["mva_dm_1"] == 1) 
+    & (df["mva_dm_2"] == 1)
