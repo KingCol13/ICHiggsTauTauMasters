3bd8d4815ccfc5a9359304ab7afe4c21858758db
KingCol13 <kingsleydgs@gmail.com>
2020-12-16 16:24:11 +0000

    makeBinaryDataset outputs binary datasets. Still need to add decay mode selection from command line.

--- makeBinaryDataset.cpp
+++ makeBinaryDataset.cpp
@@ -11,7 +11,7 @@ TreePlayer
 
 TODO: 
 -check keys are in file: https://root.cern/manual/storing_root_objects/#finding-tkey-objects
--split argv into int variables and float variables
+-apply selections
 -output to binary
 
 */
@@ -21,6 +21,7 @@ TODO:
 #include <vector>
 
 #include "TFile.h"
+#include "TKey.h"
 #include "TTreeReader.h"
 #include "TTreeReaderValue.h"
 
@@ -30,36 +31,80 @@ int main(int argc, char *argv[])
 	{
 		std::cerr << "Usage: makeBinaryDataset [ntupleKey1] [ntupleKey2] ..." << std::endl;
 		return -1;
-	}	
+	}
+	
+	int tau_dm_1 = 1;
+	int tau_dm_2 = 1;
 	
 	// Open file and make sure it isn't borked
-	auto myFile = TFile::Open("MVAFILE_AllHiggs_tt.root");
-	if(!myFile || myFile->IsZombie())
+	auto inFile = TFile::Open("MVAFILE_AllHiggs_tt.root");
+	if(!inFile || inFile->IsZombie())
 	{
 		std::cerr << "File didn't load correctly." << std::endl;
 		return -1;
 	}
+	
+	/*
+	TODO: check argv keys are in NTuple
+	// Make sure all keys are in file
+	TIter next(inFile->GetListOfKeys());
+	TKey *key;
+	while((key)=(TKey*)next())
+	{
+		std::cout << "Key: " << key->GetName() << " has objects of class: " << key->GetSeekKey() << std::endl;
+	}
+	*/
+	
 	// Make the reader object to iterate over
-	TTreeReader reader("ntuple", myFile);
+	TTreeReader reader("ntuple", inFile);
 	
-	// Make sure 
-	
+	// Make output value readers bound to the reader
 	std::vector<TTreeReaderValue<double>> readerValueVec;
 	for(unsigned int i=1; i<argc; i++)
 	{
 		readerValueVec.emplace_back(reader, argv[i]);
 	}
 	
-	for(unsigned int i=0; i<10; i++)
+	// Make selector value readers bound to the reader
+	// Tau decay mode selectors:
+	TTreeReaderValue<int> mva_dm_1(reader, "mva_dm_1");
+	TTreeReaderValue<int> mva_dm_2(reader, "mva_dm_1");
+	TTreeReaderValue<int> hps_dm_1(reader, "tau_decay_mode_1");
+	TTreeReaderValue<int> hps_dm_2(reader, "tau_decay_mode_2");
+	// Neutrino momenta selectors:
+	TTreeReaderValue<double> gen_nu_p_1(reader, "gen_nu_p_1");
+	TTreeReaderValue<double> gen_nu_p_2(reader, "gen_nu_p_2");
+	
+	// Binary file stream for output
+	std::fstream outFile;
+	outFile.open("recordData.dat", std::ios::binary);
+	
+	//for(unsigned int i=0; i<10; i++)
+	while(reader.Next())
 	{
 		reader.Next();
-		// loop through values to output
-		for(unsigned int j=0; j<argc-1; j++)
+		// Selection conditions
+		if( 
+			(*mva_dm_1 == tau_dm_1) &&
+			(*mva_dm_2 == tau_dm_2) &&
+			(*hps_dm_1 == tau_dm_1) &&
+			(*hps_dm_2 == tau_dm_2) &&
+			(*gen_nu_p_1 > -9000)   &&
+			(*gen_nu_p_2 > -9000)
+		)
 		{
-			 std::cout << *readerValueVec[j] << ", ";
+			// loop through values to output
+			for(unsigned int j=0; j<argc-1; j++)
+			{
+				float val = *readerValueVec[j];
+				outFile.write( (char *) &val, sizeof(float) );
+			}
 		}
-		std::cout << std::endl;
 	}
+	
+	std::cout << "Bytes per entry: " << sizeof(float)*readerValueVec.size() << std::endl;
+	outFile.close();
+	inFile->Close();
 	
 	return 0;
 }
